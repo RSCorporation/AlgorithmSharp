9501720ad036b741922e8384eac3b2f34331eec5
Roman <roma-svistunov@tut.by>
2019-08-03 17:59:57 +0300

    Update Delegates.cs

--- AlgorithmSharp/AlgorithmSharp/Utils/Delegates.cs
+++ AlgorithmSharp/AlgorithmSharp/Utils/Delegates.cs
@@ -1,29 +1,23 @@
 ﻿namespace AlgorithmSharp.Utils
 {
     /// <summary>
-    ///     Class to store basic delegates
+    ///     Represents an associative operation
     /// </summary>
-    public static class Delegates
-    {
-        /// <summary>
-        ///     Represents an associative operation
-        /// </summary>
-        public delegate T AssociativeOperation<T>(T leftOperand, T rightOperand);
+    public delegate T AssociativeOperation<T>(T leftOperand, T rightOperand);
 
-        /// <summary>
-        ///     Represents commutative operation
-        /// </summary>
-        public delegate TOut CommutativeOperation<out TOut, in TIn>(TIn leftOperand, TIn rightOperand);
+    /// <summary>
+    ///     Represents commutative operation
+    /// </summary>
+    public delegate TOut CommutativeOperation<out TOut, in TIn>(TIn leftOperand, TIn rightOperand);
 
-        /// <summary>
-        ///     Represents idempotent operation
-        /// </summary>
-        public delegate T IdempotentOperation<T>(T left, T right);
+    /// <summary>
+    ///     Represents idempotent operation
+    /// </summary>
+    public delegate T IdempotentOperation<T>(T left, T right);
 
-        /// <summary>
-        ///     Represents any operation
-        /// </summary>
-        public delegate TOut Operation<out TOut, in TLeftOperand, in TRightOperand>(TLeftOperand leftOperand,
-            TRightOperand rightOperand);
-    }
+    /// <summary>
+    ///     Represents any operation
+    /// </summary>
+    public delegate TOut Operation<out TOut, in TLeftOperand, in TRightOperand>(TLeftOperand leftOperand,
+        TRightOperand rightOperand);
 }
