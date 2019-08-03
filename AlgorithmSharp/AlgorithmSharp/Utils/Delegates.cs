6ee50aabb5196ef7d3d4ed15ed3644a63294269a
Roman <roma-svistunov@tut.by>
2019-08-03 17:56:45 +0300

    BasicDelegates

---
+++ AlgorithmSharp/AlgorithmSharp/Utils/Delegates.cs
@@ -0,0 +1,29 @@
+﻿namespace AlgorithmSharp.Utils
+{
+    /// <summary>
+    ///     Class to store basic delegates
+    /// </summary>
+    public static class Delegates
+    {
+        /// <summary>
+        ///     Represents an associative operation
+        /// </summary>
+        public delegate T AssociativeOperation<T>(T leftOperand, T rightOperand);
+
+        /// <summary>
+        ///     Represents commutative operation
+        /// </summary>
+        public delegate TOut CommutativeOperation<out TOut, in TIn>(TIn leftOperand, TIn rightOperand);
+
+        /// <summary>
+        ///     Represents idempotent operation
+        /// </summary>
+        public delegate T IdempotentOperation<T>(T left, T right);
+
+        /// <summary>
+        ///     Represents any operation
+        /// </summary>
+        public delegate TOut Operation<out TOut, in TLeftOperand, in TRightOperand>(TLeftOperand leftOperand,
+            TRightOperand rightOperand);
+    }
+}
